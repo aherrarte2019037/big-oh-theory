a93146031c5c3823143bfe9c88da7d2eb7c6d250
Angel Herrarte <angelherrarte3@gmail.com>
2024-10-05 15:38:19 -0600

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,3 +1,5 @@
+from profiler import profile_function
+
 def function(n):
     counter = 0
     i = n // 2
@@ -10,3 +12,9 @@ def function(n):
                 k *= 2
             j += 1
         i += 1
+
+n_values = [1, 10, 100, 1000, 10000, 100000, 1000000]
+
+for n in n_values:
+    print(f"|--------------------n = {n}---------------------|")
+    profile_function(lambda: function(n))

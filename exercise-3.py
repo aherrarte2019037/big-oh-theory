a01df40b620e79a5f83dff55e44a923cced8c58a
Angel Herrarte <angelherrarte3@gmail.com>
2024-10-05 15:38:54 -0600

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,4 +1,12 @@
+from profiler import profile_function
+
 def function(n):
     for i in range(1, n // 3 + 1):
         for j in range(1, n + 1, 4):
             print("Sequence")
+
+n_values = [1, 10, 100, 1000, 10000, 100000, 1000000]
+
+for n in n_values:
+    print(f"|--------------------n = {n}---------------------|")
+    profile_function(lambda: function(n))
